0da541df5f30c32b5bc07ae42ec45a1991e8fc1b
voyager003 <miraclex000@gmail.com>
2023-03-16 16:22:47 +0900

    비율 할인 정책 구현체 : PercentDiscountPolicy

---
+++ src/main/java/chapter_2/PercentDiscountPolicy.java
@@ -0,0 +1,16 @@
+package chapter_2;
+
+public class PercentDiscountPolicy extends DiscountPolicy {
+
+    private double percent;
+
+    public PercentDiscountPolicy(double percent, DiscountCondition... conditions) {
+        super(conditions);
+        this.percent = percent;
+    }
+
+    @Override
+    protected Money getDiscountAomunt(Screening screening) {
+        return screening.getMovieFee().times(percent);
+    }
+}
