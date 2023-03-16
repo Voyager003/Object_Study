6ec0172dd54a97254b7c7b8ff88f01aa3b7c75e2
voyager003 <miraclex000@gmail.com>
2023-03-16 16:21:21 +0900

    금액 할인 정책 구현체 : AomountDiscountPolicy

--- src/main/java/chapter_2/AmountDiscountPolicy.java
+++ src/main/java/chapter_2/AmountDiscountPolicy.java
@@ -1,2 +1,16 @@
-package chapter_2;public class AmountDiscountPolicy {
+package chapter_2;
+
+public class AmountDiscountPolicy extends DiscountPolicy {
+
+    private Money discountAmount;
+
+    public AmountDiscountPolicy(Money discountAmount, DiscountCondition... conditions) {
+        super(conditions);
+        this.discountAmount = discountAmount;
+    }
+
+    @Override
+    protected Money getDiscountAomunt(Screening screening) {
+        return discountAmount;
+    }
 }
