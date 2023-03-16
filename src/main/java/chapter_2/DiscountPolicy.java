20d05c2a6d68615c6eaf4b671ea5860ee5cb6b42
voyager003 <miraclex000@gmail.com>
2023-03-16 16:00:28 +0900

    할인 정책을 구현한 class : DiscountPolicy 하나의 할인 정책은 여러 할인 조건을 포함한다

--- src/main/java/chapter_2/DiscountPolicy.java
+++ src/main/java/chapter_2/DiscountPolicy.java
@@ -1,2 +1,25 @@
-package chapter_2;public class DiscountPolicy {
+package chapter_2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class DiscountPolicy {
+
+    private List<DiscountCondition> conditions = new ArrayList<>();
+
+    public DiscountPolicy(DiscountCondition ... conditions) {
+        this.conditions = Arrays.asList(conditions);
+    }
+
+    public Money caculateDiscountAmount(Screening screening) {
+        for (DiscountCondition each : conditions) {
+            if (each.isSatisfiedBy(screening)) {
+                return getDiscountAomunt(screening);
+            }
+        }
+        return Money.ZERO;
+    }
+
+    abstract protected Money getDiscountAomunt(Screening screening);
 }
