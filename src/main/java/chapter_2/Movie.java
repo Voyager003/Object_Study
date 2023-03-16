93f49b214c8c9f3968df946ac77e817bf71ded44
voyager003 <miraclex000@gmail.com>
2023-03-16 15:52:01 +0900

    상영 영화의 정보를 담은 class : Movie

--- src/main/java/chapter_2/Movie.java
+++ src/main/java/chapter_2/Movie.java
@@ -1,2 +1,25 @@
-package chapter_2;public class Movie {
+package chapter_2;
+
+// '상영'할 영화의 정보
+public class Movie {
+
+    private String title;
+    private Dutation runningTime;
+    private Money fee;
+    private DiscountPolicy discountPolicy;
+
+    public Movie(String title, Dutation runningTime, Money fee, DiscountPolicy discountPolicy) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.fee = fee;
+        this.discountPolicy = discountPolicy;
+    }
+
+    public Money getFee() {
+        return fee;
+    }
+
+    public Money calculateMovieFee(Screening screening) {
+        return fee.minus(discountPolicy.calculateDiscountAmount(screening));
+    }
 }
