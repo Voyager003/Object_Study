dc42b583be5131cc795ffe36982d09d75a30b7f0
voyager003 <miraclex000@gmail.com>
2023-03-16 15:21:55 +0900

    영화 '상영'을 구현한 class 상영활 영화, 순서, 상영 시작시간을 변수로 가진다

---
+++ src/main/java/chapter_2/Screening.java
@@ -0,0 +1,29 @@
+package chapter_2;
+
+import java.time.LocalDateTime;
+
+// 관람객들이 예매하는 대상인 영화 '상영'을 구현 class
+public class Screening {
+
+    private Movie movie;
+    private int sequence;
+    private LocalDateTime whenScreened;
+
+    public Screening(Movie movie, int sequence, LocalDateTime whenScreened) {
+        this.movie = movie;
+        this.sequence = sequence;
+        this.whenScreened = whenScreened;
+    }
+
+    public LocalDateTime getWhenScreened() {
+        return whenScreened;
+    }
+
+    public boolean isSequence(int sequence) {
+        return this.sequence == sequence;
+    }
+
+    public Money getMovieFee() {
+        return movie.getFee();
+    }
+}
