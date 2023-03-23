5154eeb7b520bea15986c67df925a9ad530e6921
voyager003 <miraclex000@gmail.com>
2023-03-23 21:26:31 +0900

    할인 정책별 요금을 계산

--- src/main/java/chapter_3/Movie.java
+++ src/main/java/chapter_3/Movie.java
@@ -15,5 +15,28 @@ public class Movie {
     private MovieType movieType;
     private Money discountAmount;
     private double discountPercent;
+
+    public Money calculateAmountDiscountedFee() {
+        if (movieType != MovieType.AMOUNT_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+        return fee.minus(discountAmount);
+    }
+
+    public Money calculatePercentDiscountedFee() {
+        if (movieType != MovieType.PERCENT_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+        return fee.minus(fee.times(discountPercent));
+    }
+
+    public Money calculateNoneDiscountedFee() {
+        if (movieType != MovieType.NONE_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+        return fee;
     }
 }
+
+
+
