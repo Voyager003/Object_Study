dce3c3adeb70fc5ff1447c7acff135ae350071b8
voyager003 <miraclex000@gmail.com>
2023-03-23 21:03:42 +0900

    고객과 요금 정보를 담은 클래스
    데이터 중심의 Movie 클래스

---
+++ src/main/java/chapter_3/Movie.java
@@ -0,0 +1,19 @@
+package chapter_3;
+
+import chapter_3.Money;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Movie {
+
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private List<DiscountCondition> discountConditions;
+
+    private MovieType movieType;
+    private Money discountAmount;
+    private double discountPercent;
+    }
+}
